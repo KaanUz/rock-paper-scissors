e521951db75b1f35a23130610c4b8dfd1ac01998
necatikaan.uzmez <necatikaan.uzmez@logosgt.com.tr>
2018-09-24 23:01:43 +0300

    Restructure the file, remove debugging content, and implement socket.io

--- web-app/server/app.js
+++ web-app/server/app.js
@@ -1,20 +1,95 @@
-var Http                = require('http');
-var QueryString         = require('querystring');
-var StringBuilder       = require('stringbuilder');
-var Util                = require('util');
-var ejs                 = require('ejs');
+var app                 = require('http').createServer(handler);
+var io                  = require('socket.io')(app);
 var fs                  = require('fs');
 
-var     port                = 9000;
-const   dirname             = 'rps/public/views'
-var     body                = "<html><head><title>%s</title></head><body>%s %s</body></html>"
+app.listen(9000);
 
-var htmlContent = fs.readFileSync(dirname + '/index.ejs', 'utf8');
-var css = {style : fs.readFileSync('rps/public/css/style.css','utf8')};
+var QueryString         = require('querystring');
+var Util                = require('util');
+var ejs                 = require('ejs');
 
+var htmlContent = fs.readFileSync('rock-paper-scissors/web-app/public/views/index.ejs', 'utf8');
 
 var contentTypeHtml     = {"Content-Type": "text/html"};
 var homeLink            = "Go to <a href='/'>Home</a>";
+
+var app = Http.createServer();
+
+const serveCSS = function (request, response) {
+    fs.readFile(__dirname + '/../public/css/style.css', function (err, data) {
+        if (err) console.log(err);
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.write(data);
+        response.end();
+      });
+};
+
+const serveImages = function(request, response, imagePath) {
+    fs.readFile(__dirname + '/..' + imagePath, function (err, content) {
+        if (err) {
+            response.writeHead(400, contentTypeHtml)
+            console.log(err);
+            response.end("No such image");    
+        } else {
+            //specify the content type in the response will be an image
+            response.writeHead(200,{'Content-type':'image/png'});
+            response.end(content);
+        }
+    })
+};
+
+function handler(request, response) {
+    // console.log(request.url);
+    switch (request.method){
+        case "GET":
+            if (request.url === "/")
+                getHome(request, response);
+            else if (request.url.indexOf('.css') != -1)
+                serveCSS(request, response);
+            else if (request.url.indexOf('.png') != -1)
+                serveImages(request, response, request.url);
+            else if
+                
+            else
+                get404(request, response); 
+            break;
+        case "POST":
+            if (request.url === "/send") {
+                var requestBody = '';
+                request.on('data', function (data) {
+                    requestBody += data;
+                    if ( requestBody.length > 5e6) {
+                        response.writeHead(
+                            413, 
+                            "Request Entity Too Large", 
+                            contentTypeHtml
+                        );
+                        response.write(
+                            Util.format(
+                                body, 
+                                '413', 
+                                'Request Entity Too Large', 
+                                homeLink 
+                            )
+                        );
+                        response.end();
+                    }
+                });
+                console.log(requestBody);
+                request.on('end', function (end) {
+                    var formData = QueryString.parse(requestBody);
+                    getCalcForm(request, response, formData);
+                });
+            } else {
+                get404(request, response);
+            }
+            break;
+        default:
+            get405(request, response);
+            break;
+    }
+    
+}
 
 function getHome(request, response) {
     response.writeHead(
@@ -25,8 +100,7 @@ function getHome(request, response) {
         ejs.render(
             htmlContent, {
                 filename: 'index.ejs', 
-                res: null,
-                css : css
+                res: null
             }
         )
     );
@@ -67,103 +141,9 @@ function get405(request, response) {
     response.end();
 }
 
-function getCalcHtml(request, response, data) {
-    var stringBuilder   = new StringBuilder({newline: "\r\n"});
-
-    stringBuilder.appendLine("<html>");
-    stringBuilder.appendLine("<body>");
-    stringBuilder.appendLine("  <form method='post'>");
-    stringBuilder.appendLine("      <table>");
-    stringBuilder.appendLine("          <tr>");
-    stringBuilder.appendLine("              <td>Enter First No : </td>");
-
-    data && data.txtFirstNo ? 
-        stringBuilder.appendLine("              <td><input type='text' id='txtFirstNo' name='txtFirstNo' value='{0}'/></td>", data.txtFirstNo):
-        stringBuilder.appendLine("              <td><input type='text' id='txtFirstNo' name='txtFirstNo' value=''/></td>");
-    
-    stringBuilder.appendLine("          </tr>");
-    stringBuilder.appendLine("          <tr>");
-    stringBuilder.appendLine("              <td>Enter Second No : </td>");
-    
-    data && data.txtSecondNo ? 
-        stringBuilder.appendLine("              <td><input type='text' id='txtSecondNo' name='txtSecondNo' value='{0}'/></td>", data.txtSecondNo):
-        stringBuilder.appendLine("              <td><input type='text' id='txtSecondNo' name='txtSecondNo' value=''/></td>");
-
-    stringBuilder.appendLine("          </tr>");
-    stringBuilder.appendLine("          <tr>");
-    stringBuilder.appendLine("              <td><input type='submit' value='Calculate'/></td>");
-    stringBuilder.appendLine("          </tr>");
-
-    if (data && data.txtFirstNo && data.txtSecondNo) {
-        var sum = parseInt(data.txtFirstNo) + parseInt(data.txtSecondNo);
-        stringBuilder.appendLine("          <tr>");
-        stringBuilder.appendLine("              <td><span>Sum = {0}</span></td>", sum);
-        stringBuilder.appendLine("          </tr>");
-    }
-
-    stringBuilder.appendLine("      </table>");
-    stringBuilder.appendLine("  </form>");
-    stringBuilder.appendLine("</body>");
-    stringBuilder.appendLine("</html>");
-
-    stringBuilder.build(function (error, result) {
-        response.write(result);
-        response.end();
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
     });
-}
-
-function getCalcForm(request, response, formData) {
-    response.writeHead(
-        200, 
-        contentTypeHtml
-    );
-    getCalcHtml(request, response, formData)
-} 
-
-var httpServer = Http.createServer(function(request, response) {
-    // console.log(request.url);
-    switch (request.method){
-        case "GET":
-            if (request.url === "/")
-                getHome(request, response);
-            else if (request.url === "/calc")
-                getCalcForm(request, response);
-            else
-                get404(request, response); 
-            break;
-        case "POST":
-            if (request.url === "/calc") {
-                var requestBody = '';
-                request.on('data', function (data) {
-                    requestBody += data;
-                    if ( requestBody.length > 5e6) {
-                        response.writeHead(
-                            413, 
-                            "Request Entity Too Large", 
-                            contentTypeHtml
-                        );
-                        response.write(
-                            Util.format(
-                                body, 
-                                '413', 
-                                'Request Entity Too Large', 
-                                homeLink 
-                            )
-                        );
-                        response.end();
-                    }
-                });
-                request.on('end', function (end) {
-                    var formData = QueryString.parse(requestBody);
-                    getCalcForm(request, response, formData);
-                });
-            } else {
-                get404(request, response);
-            }
-            break;
-        default:
-            get405(request, response);
-            break;
-    }
-    
-}).listen(port)
+  });
