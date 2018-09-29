33188bed83a4e259151701be1f7be6f2cff945cf
necatikaan.uzmez <necatikaan.uzmez@logosgt.com.tr>
2018-09-29 15:35:38 +0300

    Refactor JQuery code into pure JS

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,13 +1,21 @@
-let usrScore        = 0;
-let aiScore         = 0;
 var socket = io('http://localhost:9000');
 
+let usrScore        = 0;
+let aiScore         = 0;
+
+var choices                 = document.getElementsByClassName('choice');
+var res_p                   = document.querySelector('.res > p');
+var usrScore_span           = document.getElementById("usr_score");
+var aiScore_span            = document.getElementById("ai_score");
+var aiChoice_div            = document.getElementsByClassName("ai-choice")[0];
+var aiChoice_div_classList  = aiChoice_div.classList;
+var aiChoice_img            = aiChoice_div.getElementsByTagName("img")[0];
+
 socket.on('connect', function () {
-    $('#message').empty();
-    $('#message').append($('<p>').text("Choose your weapon!"));
+    console.log('Socket Connection Established...')
+    res_p.innerHTML = "Choose your weapon!";
 });
 
-var choices = document.getElementsByClassName('choice');
 for (let j = 0; j < choices.length; j++) {
     let choice = choices[j];
     choice.addEventListener('click', function() {
@@ -18,36 +26,36 @@ for (let j = 0; j < choices.length; j++) {
 }
 
 socket.on('game result', function(data) {
-    $('#message').empty();
-    $('#message').append($('<p>').text(data.msg));
-        
-    aiChoice_img = $('#ai_choice > img')
-    aiChoice_img.removeAttr('style');
-    $('#ai_choice > img').attr("src", "public/images/" + data.aiChoice + ".png")
+    var usrChoice_div_classList = document.getElementById(data.usrChoice).classList;
+
+    res_p.innerHTML = data.msg;
+    
+    aiChoice_img.removeAttribute('style');
+    aiChoice_img.setAttribute("src", "/public/images/" + data.aiChoice + ".png")
 
     if (data.res == null) {
-        $(`#${data.usrChoice}`).addClass('gray-glow');
-        $(`#ai_choice`).addClass('gray-glow');
+        usrChoice_div_classList.add('gray-glow');
+        aiChoice_div_classList.add('gray-glow');
         setTimeout( function() {
-            $(`#${data.usrChoice}`).removeClass('gray-glow');
-            $(`#ai_choice`).removeClass('gray-glow');
+            usrChoice_div_classList.remove('gray-glow');
+            aiChoice_div_classList.remove('gray-glow');
         }, 500); 
     } else if (data.res) {
-        $(`#usr_score`).html(++usrScore);
-        $(`#${data.usrChoice}`).addClass('green-glow');
-        $(`#ai_choice`).addClass('red-glow');
+        usrScore_span.innerHTML = ++usrScore;
+        usrChoice_div_classList.add("green-glow");
+        aiChoice_div_classList.add("red-glow");
         setTimeout( function() {
-            $(`#${data.usrChoice}`).removeClass('green-glow');
-            $(`#ai_choice`).removeClass('red-glow');
+            usrChoice_div_classList.remove('green-glow');
+            aiChoice_div_classList.remove('red-glow');
         }, 500); 
     } else {
-        $(`#ai_score`).html(++aiScore);
-        $(`#${data.usrChoice}`).addClass('red-glow');
-        $(`#ai_choice`).addClass('green-glow');
+        aiScore_span.innerHTML = ++aiScore;
+        usrChoice_div_classList.add("red-glow");
+        aiChoice_div_classList.add("green-glow");
         setTimeout( function() {
-            $(`#${data.usrChoice}`).removeClass('red-glow');
-            $(`#ai_choice`).removeClass('green-glow');
-        }, 500); 
+            usrChoice_div_classList.remove('red-glow');
+            aiChoice_div_classList.remove('green-glow');
+        }, 500);  
     }
     
 });
